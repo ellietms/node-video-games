45d472beb537dda545268d2a40fa926b32a8fb90
ellie <ellie.tms1371@gmail.com>
2020-06-23 00:21:34 +0100

    add games

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const games = require("./games.json");
 const app = express();
-app.get("/",(req,res) => {
-    console.log("just check")
-    res.send("welcome to video games");
-})
 
-app.listen(1234,() => console.log("Welcome to video games"))
+app.get("/", (req, res) => {
+  console.log("just check");
+  res.send("welcome to video games");
+});
+
+app.get("/games", (req, res) => {
+  
+  res.send(games);
+});
+
+app.listen(1234, () => console.log("Welcome to video games"));
 console.log("Hello Server");
