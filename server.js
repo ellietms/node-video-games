d8bafc1c9971e1438de2bdb970a8551aa5fb2359
ellie <ellie.tms1371@gmail.com>
2020-06-23 00:41:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/games", (req, res) => {
-  
   res.send(games);
 });
+function handleAboutReq(req, res) {
+  res.send("<h1>This page is running by Ellie</h1>");
+}
+app.get("/about", handleAboutReq);
 
 app.listen(1234, () => console.log("Welcome to video games"));
 console.log("Hello Server");
